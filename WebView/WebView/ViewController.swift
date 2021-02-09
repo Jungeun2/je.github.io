7c13760334fa51671c76d2dffbbcd2a066b7ffc7
Jungeun2 <45205109+Jungeun2@users.noreply.github.com>
2021-02-10 07:55:48 +0900

    Add files via upload

---
+++ WebView/WebView/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by 이정은 on 2020/11/11.
+//  Copyright © 2020 Jungeun Lee. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet var txtSite1: UIButton!
+    @IBOutlet var txtSite2: UIButton!
+    
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myWebView.navigationDelegate = self
+        
+        txtSite1.setTitle("Site1", for: .normal)
+        txtSite2.setTitle("Site2", for: .normal)
+        loadWebPage("https://coronaboard.kr/")
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    func checkUrl(_ url: String) -> String {
+        var strUrl = url
+        let flag = strUrl.hasPrefix("http://")
+        if !flag {
+            strUrl = "http://" + strUrl
+        }
+        return strUrl
+    }
+
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+        let myUrl = checkUrl(txtUrl.text!)
+        txtUrl.text = ""
+        loadWebPage(myUrl)
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+        loadWebPage("https://www.wdgbook.com/")
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+        loadWebPage("https://www.wdgbook.com/page/chungdam")
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+        let htmlString = "<h1> HTML String </h1><p> String 변수를 이용한 웹 페이지 </p><p><a href=\"http://2sam.net\">2sma</a>으로 이동</p>"
+        myWebView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+        myWebView.stopLoading()
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myWebView.reload()
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack()
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward()
+    }
+    
+}
+
